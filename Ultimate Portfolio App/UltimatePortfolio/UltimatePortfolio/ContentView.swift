967121841533e2db8038c1e74d777c1251a39c26
KelsCree <kelseycreehan@gmail.com>
2021-02-08 17:08:35 -0700

    Set Up data models.

---
+++ Ultimate Portfolio App/UltimatePortfolio/UltimatePortfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UltimatePortfolio
+//
+//  Created by Kelsey Creehan on 2/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ultimate Portfolio App/UltimatePortfolio/UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,28 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Kelsey Creehan on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    
+    @StateObject var dataController: DataController
+    
+    init() {
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
+                
+        }
+    }
+}
